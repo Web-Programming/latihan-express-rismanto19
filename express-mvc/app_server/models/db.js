64c6e076ea3bb8762bd5bd4ad40001ce6b57ad54
rismanto <rismanto685@gmail.com>
2024-10-23 09:33:04 +0700

    update

--- express-mvc/app_server/models/db.js
+++ express-mvc/app_server/models/db.js
@@ -1,15 +1,16 @@
 let mongoose = require("mongoose");
-let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
-require("./mahasiswa");
+let dbURI = "mongodb://localhost:27017/serverPAW";
+appName=paw
 mongoose.connect(dbURI,{
     //useNewUrparser: true
-})
+});
 mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB");
-})
+});
 mongoose.connection.on("error", (error)=>{
     console.log("Connection Error : " + error);
 });
 mongoose.connection.on("disconnected", () => {
     console.log("Disconnected from MongoDB");
 });
+require("./mahasiswa");
--- express-mvc/app_server/routes/mahasiswa.js
+++ express-mvc/app_server/routes/mahasiswa.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-// Tambahkan rute untuk /mahasiswa/index
-router.get('/index', function(req, res, next) {
-  res.send('Halaman Index Mahasiswa');
-});
+
 // route untuk mendaftarkan URL dari setiap request yang sebelumnya telah dibuat pada controller, antara lain index, insert, uodate, show dan destroy
 const mhsController = require('../controllers/mahasiswa');
+
 router.get("/", mhsController.index); //list mahasiswa
 router.post("/insert", mhsController.insert); //insert mahasiswa
 router.patch("/update/:id", mhsController.update); //mengupdate mahasiswa
